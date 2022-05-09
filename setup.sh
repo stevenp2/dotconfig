0a1891a6898e64e5c30c1e5bcc8504f35ce0cd09
s <s.phung808@gmail.com>
2022-05-09 16:22:52 +1000

    base to work with

---
+++ setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-0 --off --output DVI-I-1 --mode 1920x1080 --pos 0x360 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 4480x360 --rotate normal --output DP-0 --off --output DVI-D-0 --off --output DP-1 --mode 2560x1440 --pos 1920x0 --rotate normal
