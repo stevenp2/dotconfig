d3c82c28445baf279208e867021bcb6029402e61
stevenp2 <s.phung808@gmail.com>
2022-05-26 19:42:46 +1000

    colourscheme added

---
+++ nvim/lua/stevenp2/colorscheme.lua
@@ -0,0 +1,10 @@
+-- local colorscheme = "default"
+
+local colorscheme = "tokyonight"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
--- nvim/lua/stevenp2/init.lua
+++ nvim/lua/stevenp2/init.lua
@@ -1,4 +1,4 @@
 require("stevenp2.keymaps")
 require("stevenp2.options")
 require("stevenp2.plugins")
-
+require("stevenp2.colorscheme")
