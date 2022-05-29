4c8109cffe29c84c53134ea4cf81c8d734cab25e
stevenp2 <s.phung808@gmail.com>
2022-05-29 17:27:37 +1000

    gitsigns plugin added

--- nvim/lua/stevenp2/plugins.lua
+++ nvim/lua/stevenp2/plugins.lua
@@ -45,6 +45,7 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "numtoStr/Comment.nvim" -- comment things easily
+  use "lewis6991/gitsigns.nvim" -- signs to keep track of file edits for git
 
   -- ColourSchemes
   use "folke/tokyonight.nvim" -- Colourscheme plugin
