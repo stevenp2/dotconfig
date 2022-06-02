3746235b3f0d7bad2cd11ad6d2790cfdc33129ea
stevenp2 <s.phung808@gmail.com>
2022-06-02 22:16:52 +1000

    ultisnips configuration

---
+++ nvim/lua/stevenp2/tex/ultisnips.lua
@@ -0,0 +1,8 @@
+return function()
+    -- Disable snipmate plugins to avoid duplicate snippets
+    vim.g.UltiSnipsEnableSnipMate = 0
+    vim.g.UltiSnipsRemoveSelectModeMappings = 0
+    vim.g.UltiSnipsExpandTrigger = "<tab>"
+    vim.g.UltiSnipsJumpForwardTrigger = "<tab>"
+    vim.g.UltiSnipsJumpBackwardTrigger = "<S-tab>"
+end
