d7a02d495b175ca3e3acb50334774a519bed3e64
stevenp2 <s.phung808@gmail.com>
2022-08-15 16:31:18 +1000

    better contrast

--- nvim/lua/stevenp2/colorscheme.lua
+++ nvim/lua/stevenp2/colorscheme.lua
@@ -1,10 +1,13 @@
 -- local colorscheme = "default"
-
 local colorscheme = "tokyonight"
+local style = "night"
 
+vim.g.tokyonight_style = style
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
+
+
