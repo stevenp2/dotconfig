c7e46beb996d6e9fc61d60339f93afe4572000fc
stevenp2 <s.phung808@gmail.com>
2022-09-04 11:39:34 +1000

    feat: glow for markdown preview

--- nvim/lua/stevenp2/plugins.lua
+++ nvim/lua/stevenp2/plugins.lua
@@ -59,6 +59,7 @@ return packer.startup(function(use)
     requires = { 'kyazdani42/nvim-web-devicons' },
     config = require("stevenp2.alpha")
     } -- greeter for nvim
+  use "ellisonleao/glow.nvim"
 
   -- ColourSchemes
   use "folke/tokyonight.nvim" -- Colourscheme plugin
@@ -76,38 +77,7 @@ return packer.startup(function(use)
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
   -- urlview - a special plugin that gets to take up a lot of space
-  use {"axieax/urlview.nvim",
-    config = function()
-      require("urlview").setup({
-        -- Prompt title (`<context> <default_title>`, e.g. `Buffer Links:`)
-        default_title = "Links:",
-        -- Default picker to display links with
-        -- Options: "native" (vim.ui.select) or "telescope"
-        default_picker = "native",
-        -- Set the default protocol for us to prefix URLs with if they don't start with http/https
-        default_prefix = "https://",
-        -- Command or method to open links with
-        -- Options: "netrw", "system" (default OS browser); or "firefox", "chromium" etc.
-        navigate_method = "system",
-        -- Ensure links shown in the picker are unique (no duplicates)
-        unique = true,
-        -- Ensure links shown in the picker are sorted alphabetically
-        sorted = true,
-        -- Logs user warnings (recommended for error detection)
-        debug = true,
-        -- Custom search captures
-        -- NOTE: captures follow Lua pattern matching (https://riptutorial.com/lua/example/20315/lua-pattern-matching)
-        custom_searches = {
-          -- KEY: search source name
-          -- VALUE: custom search function or table (map with keys capture, format)
-          jira = {
-            capture = "AXIE%-%d+",
-            format = "https://jira.axieax.com/browse/%s",
-          },
-        },
-      })
-    end
-  }
+  use "axieax/urlview.nvim"
 
   -- lsp
   use "neovim/nvim-lspconfig" -- enable lsp
