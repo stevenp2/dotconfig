5d19ced86d1e22b4bc6f366aee949dd1803f7968
Steven Phung <sphung4035510@gmail.com>
2023-02-13 15:40:47 +1100

    remove deprecated function

--- nvim/lua/stevenp2/plugins/utils/nvim-tree.lua
+++ nvim/lua/stevenp2/plugins/utils/nvim-tree.lua
@@ -49,12 +49,6 @@ nvim_tree.setup {
   },
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
