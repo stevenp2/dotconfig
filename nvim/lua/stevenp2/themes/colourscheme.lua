b13f9b82cbe73583b034e1a953ce1994b246d209
Steven Phung <sphung4035510@gmail.com>
2023-02-17 16:44:25 +1100

    use as M and move back to original location

---
+++ nvim/lua/stevenp2/themes/colourscheme.lua
@@ -0,0 +1,29 @@
+local M = {}
+
+function M.setup()
+  require("tokyonight").setup({
+    style = "storm", -- night|storm|day
+    transparent = false,
+    terminal_colors = true,
+    styles = {
+      comments = "italic",
+      keywords = "italic",
+      functions = "NONE",
+      variables = "NONE",
+      sidebars = "dark",
+      floats = "dark",
+    },
+    sidebars = { "qf", "help" },
+    day_brightness = 0.3,
+    hide_inactive_statusline = false,
+    dim_inactive = false,
+    lualine_bold = false,
+    on_colors = function(colors) end,
+    on_highlights = function(highlights, colors) end,
+  })
+
+  local colourscheme = "tokyonight"
+  vim.cmd("colorscheme " .. colourscheme)
+end
+
+return M
