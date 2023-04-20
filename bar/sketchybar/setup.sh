2089651a206254ed35cf02b09dcf80528afcc422
Steven Phung <sphung4035510@gmail.com>
2023-04-20 20:12:19 +1000

    download sketchybar-app-font as part of setup

--- bar/sketchybar/setup.sh
+++ bar/sketchybar/setup.sh
@@ -5,7 +5,8 @@ action="install sketchybar"
 if confirm "$action"; then
   brew tap FelixKratz/formulae && \
   brew install sketchybar && \
-  brew services start sketchybar
+  brew services start sketchybar && \
+  curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.4/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
 fi
 
 action="link config"
