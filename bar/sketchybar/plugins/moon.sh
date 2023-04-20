7c58cfeac8cd1b5d2a5d50bf743b35ef70def1c2
Steven Phung <sphung4035510@gmail.com>
2023-04-20 20:00:40 +1000

    config edited from base https://github.com/FelixKratz/dotfiles/tree/master/.config/sketchybar

---
+++ bar/sketchybar/plugins/moon.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+case "$SENDER" in
+  "mouse.clicked") 
+    if [ $MODIFIER = "shift" ]; then
+      open "https://www.lichess.org/"
+    else
+      open "https://www.reddit.com/r/all/"
+    fi
+esac
