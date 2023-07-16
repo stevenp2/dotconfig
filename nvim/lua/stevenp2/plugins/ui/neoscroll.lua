9b8aef0b1a788e4c146ba66410ec3547c95070a9
Steven Phung <sphung4035510@gmail.com>
2023-07-16 23:35:25 +1000

    remove smooth scrolling for c-y and c-e

--- nvim/lua/stevenp2/plugins/ui/neoscroll.lua
+++ nvim/lua/stevenp2/plugins/ui/neoscroll.lua
@@ -9,8 +9,7 @@ function M.setup()
   
   scroll.setup({
     -- All these keys will be mapped to their corresponding default scrolling animation
-    mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
-                '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
+    mappings = {'<C-u>', '<C-d>', 'zz'},
     hide_cursor = true,          -- Hide cursor while scrolling
     stop_eof = true,             -- Stop at <EOF> when scrolling downwards
     respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
@@ -20,23 +19,6 @@ function M.setup()
     post_hook = nil,             -- Function to run after the scrolling animation ends
     performance_mode = false,    -- Disable "Performance Mode" on all buffers.
   })
-  
-  -- remapping scroll function 
-  --[[ local t = {} ]]
-  --[[ -- Syntax: t[keys] = {function, {function arguments}} ]]
-  --[[ t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '250'}} ]]
-  --[[ t['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '250'}} ]]
-  --[[ t['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '450'}} ]]
-  --[[ t['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '450'}} ]]
-  --[[ t['<C-y>'] = {'scroll', {'-0.10', 'false', '100'}} ]]
-  --[[ t['<C-e>'] = {'scroll', { '0.10', 'false', '100'}} ]]
-  --[[ t['zt']    = {'zt', {'250'}} ]]
-  --[[ t['zz']    = {'zz', {'250'}} ]]
-  --[[ t['zb']    = {'zb', {'250'}} ]]
-  --[[]]
-  --[[ require('neoscroll.config').set_mappings(t) ]]
-
-
 end
 
 return M
