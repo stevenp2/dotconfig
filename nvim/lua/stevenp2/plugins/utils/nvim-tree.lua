c212bd6c216eefdd110db9016c604add661c84a8
Steven Phung <sphung4035510@gmail.com>
2023-12-17 17:56:52 +1100

    fix nvim_tree error starting

--- nvim/lua/stevenp2/plugins/utils/nvim-tree.lua
+++ nvim/lua/stevenp2/plugins/utils/nvim-tree.lua
@@ -106,22 +106,6 @@ function M.setup()
     return
   end
 
-  local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-  if not config_status_ok then
-    return
-  end
-
-  -- Replaces auto_close
-  local tree_cb = nvim_tree_config.nvim_tree_callback
-  vim.api.nvim_create_autocmd("BufEnter", {
-    nested = true,
-    callback = function()
-      if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
-        vim.cmd "quit"
-      end
-    end
-  })
-
   nvim_tree.setup({
     renderer = {
       icons = {
@@ -153,7 +137,7 @@ function M.setup()
     hijack_cursor = false,
     update_cwd = true,
     diagnostics = {
-      enable = true,
+      enable = false,
       icons = {
         hint = "",
         info = "",
@@ -178,16 +162,6 @@ function M.setup()
       enable = true,
       ignore = true,
       timeout = 500,
-    },
-    view = {
-      width = 30,
-      hide_root_folder = false,
-      side = "left",
-      mappings = {
-        custom_only = false,
-      },
-      number = false,
-      relativenumber = false,
     },
     trash = {
       cmd = "trash",
