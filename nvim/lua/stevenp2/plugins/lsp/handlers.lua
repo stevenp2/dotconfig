8096347b80ec917bfa3279204f0658606713e8ae
Steven Phung <sphung4035510@gmail.com>
2024-05-03 11:54:07 +1000

    move irrelevant keymap in lsp keymappings to correct place

--- nvim/lua/stevenp2/plugins/lsp/handlers.lua
+++ nvim/lua/stevenp2/plugins/lsp/handlers.lua
@@ -74,7 +74,6 @@ local function lsp_keymaps(bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", '<cmd>lua vim.diagnostic.open_float({ border = "rounded" })<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>TroubleToggle<CR>", opts)
   vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 end
 
