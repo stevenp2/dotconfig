f8937216986fb06874217d376658a8267718282a
Steven Phung <sphung4035510@gmail.com>
2024-05-27 14:33:18 +1000

    add custom tags for CFN

---
+++ nvim/lua/stevenp2/plugins/lsp/settings/yamlls.lua
@@ -0,0 +1,33 @@
+return {
+  settings = {
+    yaml = {
+      format = {
+        enable = true,
+      },
+      hover = true,
+      completion = true,
+      customTags = {
+        "!fn",
+        "!And",
+        "!If",
+        "!Not",
+        "!Equals",
+        "!Or",
+        "!FindInMap sequence",
+        "!Base64",
+        "!Cidr",
+        "!Ref",
+        "!Ref Scalar",
+        "!Sub",
+        "!GetAtt",
+        "!GetAZs",
+        "!ImportValue",
+        "!ImportValue Scalar",
+        "!ImportValue sequence",
+        "!Select",
+        "!Split",
+        "!Join sequence"
+      },
+    },
+  },
+}
