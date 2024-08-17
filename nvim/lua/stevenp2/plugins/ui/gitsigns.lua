e43fd65e517884cbc54c4d9f395100a788169933
Steven Phung <s.phung808@gmail.com>
2024-08-17 15:26:56 +1000

    remove deprecated configs

--- nvim/lua/stevenp2/plugins/ui/gitsigns.lua
+++ nvim/lua/stevenp2/plugins/ui/gitsigns.lua
@@ -9,11 +9,11 @@ local status_ok, gitsigns = pcall(require, "gitsigns")
 
   gitsigns.setup {
     signs = {
-      add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-      change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-      delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-      topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-      changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = {text = "▎" },
     },
     signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
     numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -31,9 +31,6 @@ local status_ok, gitsigns = pcall(require, "gitsigns")
       delay = 1000,
       ignore_whitespace = false,
     },
-    current_line_blame_formatter_opts = {
-      relative_time = false,
-    },
     sign_priority = 6,
     update_debounce = 100,
     status_formatter = nil, -- Use default
@@ -45,9 +42,6 @@ local status_ok, gitsigns = pcall(require, "gitsigns")
       relative = "cursor",
       row = 0,
       col = 1,
-    },
-    yadm = {
-      enable = false,
     },
     trouble = false, -- decouple trouble.nvim and gitsigns.nvim
   }
