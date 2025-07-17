f2a6c1379067758497688008683c21071d093d02
steven <s.phung808@gmail.com>
2025-07-17 22:07:31 +1000

    update treesitter.lua with autopairs config, add autopairs config

--- nvim/lua/stevenp2/treesitter.lua
+++ nvim/lua/stevenp2/treesitter.lua
@@ -3,6 +3,9 @@ configs.setup {
   ensure_installed = "all", -- one of 'all', 'maintained' (parsers with amintainers), or a list of languages
   sync_install = false,  -- install languages synchornously (only applied to 'ensure_installed')
   ignore_install = { "" }, -- List of parsers to ignore installing
+  autopairs = {
+    enable = true,
+  },
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = { "" }, -- list of language that will be disabled
