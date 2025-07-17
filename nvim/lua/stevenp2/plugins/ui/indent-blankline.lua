dd23e5310c627813a1fbcb73eff4c6bbd3f9aee9
steven <s.phung808@gmail.com>
2025-07-17 22:10:31 +1000

    remove deprecated configs

--- nvim/lua/stevenp2/plugins/ui/indent-blankline.lua
+++ nvim/lua/stevenp2/plugins/ui/indent-blankline.lua
@@ -1,21 +1,24 @@
 local M = {}
 
 function M.setup()
-  local status_indent_ok, indent_ok = pcall(require, "indent_blankline")
+  local status_indent_ok, indent_ok = pcall(require, "ibl")
   
   if not status_indent_ok then
     return
   end
-  
+
   vim.opt.list = true
   vim.opt.listchars:append "space:⋅"
-  --[[ vim.opt.listchars:append "eol:↴" ]]
-  
+
   indent_ok.setup({
-    show_end_of_line = true,
-    space_char_blankline = " ",
-    show_current_context_start = true,
-    show_current_context = true,
+    whitespace = {
+      remove_blankline_trail = false
+    },
+    scope = {
+      enabled = true,
+      show_start = true,
+      show_end = true,
+    }
   })
 end
 
