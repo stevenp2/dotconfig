61e0b79fe81b822e5a4f671ed8926ca6408c3204
steven <s.phung808@gmail.com>
2025-07-17 22:08:30 +1000

    update for lazy.nvim

--- nvim/lua/stevenp2/plugins/utils/init.lua
+++ nvim/lua/stevenp2/plugins/utils/init.lua
@@ -3,5 +3,4 @@ require("stevenp2.plugins.utils.nvim-tree")
 require("stevenp2.plugins.utils.telescope")
 require("stevenp2.plugins.utils.toggleterm")
 require("stevenp2.plugins.utils.urlview")
-require("stevenp2.plugins.utils.doc_preview")
---[[ require("stevenp2.plugins.utils.which-key") ]]
+require("stevenp2.plugins.utils.which-key")
