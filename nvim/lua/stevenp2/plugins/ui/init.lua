9cfc31081efe099043f72eb4a53144b210e3ad20
steven <s.phung808@gmail.com>
2025-07-17 22:10:46 +1000

    remove bufferline

--- nvim/lua/stevenp2/plugins/ui/init.lua
+++ nvim/lua/stevenp2/plugins/ui/init.lua
@@ -1,6 +1,5 @@
 require("stevenp2.plugins.ui.alpha")
 require("stevenp2.plugins.ui.gitsigns")
-require("stevenp2.plugins.ui.bufferline")
 require("stevenp2.plugins.ui.scrollbar")
 require("stevenp2.plugins.ui.neoscroll")
 require("stevenp2.plugins.ui.indent-blankline")
