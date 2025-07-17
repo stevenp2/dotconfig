3462680c365560650a14206e4e66462a584ce1f1
steven <s.phung808@gmail.com>
2025-07-17 22:10:31 +1000

    sketchybar setup for mac

---
+++ bar/sketchybar/icons.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export BATTERY=
+export CPU=
+export DISK=
+export MEMORY=﬙
+export NETWORK=
+export NETWORK_DOWN=
+export NETWORK_UP=
---
+++ bar/sketchybar/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source "$HOME/dotconfig/setup-utils.sh"
+
+action="install sketchybar"
+if confirm "$action"; then
+  brew tap FelixKratz/formulae && \
+  brew install sketchybar && \
+  brew services start sketchybar
+fi
+
+action="link config"
+if confirm "$action"; then
+  link_config "$HOME/dotconfig/bar/sketchybar" "$HOME/.config"
+fi
+
+action="give execution permission to plugins"
+if confirm "$action"; then
+    chmod +x "$HOME/dotconfig/bar/sketchybar/plugins/"*
+fi
