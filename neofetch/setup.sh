a0eda0920620c808bb5c7df4d90c3592658b58f2
steven <s.phung808@gmail.com>
2025-07-17 22:08:29 +1000

    remove previous arcolinux images and add setup

---
+++ neofetch/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source "$HOME/dotconfig/setup-utils.sh"
+
+action="install neofetch"
+if ! check_dependency "neofetch" && confirm "$action"; then
+  if is_linux; then
+    sudo apt install neofetch
+  elif is_mac; then
+    brew install neofetch
+  else
+    echo "Failed to $action: unsupported OS"
+  fi
+fi
+
+action="link neofetch"
+if confirm "$action"; then
+  if is_linux || is_mac; then
+    link_config "$HOME/dotconfig/neofetch/config.conf" "$HOME/.config/neofetch/config.conf"
+  else
+    echo "invalid OSTYPE $OSTYPE for zsh setup"
+  fi
+fi
+
