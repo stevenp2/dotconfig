01fe17a4bc883b0de8e84f6b469a47921e96a2e9
steven <s.phung808@gmail.com>
2025-07-17 22:08:30 +1000

    lazy load certain plugins

--- nvim/lua/stevenp2/plugins/init.lua
+++ nvim/lua/stevenp2/plugins/init.lua
@@ -21,11 +21,56 @@ if not status_ok then
   return
 end
 
+-- lazy config
+local lazy_config = {
+  root = vim.fn.stdpath("data") .. "/lazy", -- directory where plugins will be installed
+  defaults = {
+    lazy = false, -- should plugins be lazy-loaded?
+  },
+  git = {
+    log = { "--since=3 days ago" }, -- show commits from the last 3 days
+    timeout = 120, -- kill processes that take more than 2 minutes
+    url_format = "https://github.com/%s.git",
+    filter = true,
+  },
+  install = {
+    missing = true,
+    colorscheme = { "tokyonight" },
+  },
+  performance = {
+    cache = {
+      enabled = true,
+    },
+    reset_packpath = true, -- reset the package path to improve startup time
+    rtp = {
+      reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
+      ---@type string[]
+      paths = {}, -- add any custom paths here that you want to includes in the rtp
+      ---@type string[] list any plugins you want to disable here
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+
+}
+
 -- Install your plugins here
 return lazy.setup({
   -- ColourSchemes
   { "folke/tokyonight.nvim",
     priority = 1000,
+    dependencies = {
+      "petertriho/nvim-scrollbar",
+      "nvim-lualine/lualine.nvim",
+    },
     config = function()
       require("stevenp2.themes.colourscheme").setup()
     end
@@ -39,33 +84,35 @@ return lazy.setup({
   }, -- comment things easily
 
   { "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
     dependencies = {
-      { "nvim-lua/plenary.nvim" } -- Useful lua functions used ny lots of plugins
+      {
+        "nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
+        "nvim-telescope/telescope-media-files.nvim", -- view media in telescope
+      } -- Useful lua functions used ny lots of plugins
     },
   }, -- a fuzzy finder
 
   { "kdheepak/lazygit.nvim",
+    cmd = "LazyGit",
     dependencies = {
       "nvim-telescope/telescope.nvim"
     }
   }, -- running lazygit in nvim
 
-  { "nvim-telescope/telescope-media-files.nvim",
-    dependencies = {
-      "nvim-telescope/telescope.nvim"
-    }
-  }, -- view media in telescope
-
   { "akinsho/toggleterm.nvim",
     version = "*",
+    cmd = "ToggleTerm",
     config = function() require("stevenp2.plugins.utils.toggleterm").setup() end
   }, -- toggle terminal
 
   { "axieax/urlview.nvim",
+    cmd = "UrlView",
     config = function() require("stevenp2.plugins.utils.urlview").setup() end
   },-- urlview - a special plugin that gets to take up a lot of space
 
   { "kyazdani42/nvim-tree.lua",
+    cmd = "NvimTreeToggle",
     config = function() require("stevenp2.plugins.utils.nvim-tree").setup() end
   }, -- file explorer
 
@@ -78,7 +125,6 @@ return lazy.setup({
   -- ui
   -------------------------------
   { "goolord/alpha-nvim",
-    dependencies = { "kyazdani42/nvim-web-devicons" },
     config = function() require("stevenp2.plugins.ui.alpha").setup() end
   }, -- greeter for nvim
 
@@ -91,11 +137,11 @@ return lazy.setup({
   }, --stevenp2.plugins.ui.lualine
 
   { "petertriho/nvim-scrollbar",
-    dependencies = "folke/tokyonight.nvim",
     config = function() require("stevenp2.plugins.ui.scrollbar").setup() end
   }, -- scrollbar
 
   { "karb94/neoscroll.nvim",
+    event = "VeryLazy",
     config = function() require("stevenp2.plugins.ui.neoscroll").setup() end
   }, -- smoother scrolling experience
 
@@ -113,6 +159,7 @@ return lazy.setup({
 
   { "stevearc/dressing.nvim",
     dependencies = "smjonas/inc-rename.nvim",
+    event = "VeryLazy",
     config = function() require("stevenp2.plugins.ui.dressing").setup() end
   }, -- wrapper around vim calls
 
@@ -120,32 +167,46 @@ return lazy.setup({
   -- lsp
   ----------------------------------
   { "neovim/nvim-lspconfig",
-    dependencies = { "williamboman/nvim-lsp-installer" }, -- simple lsp installer
-    priority = 1000,
     config = function()
       require("stevenp2.plugins.lsp.lspconfig").setup()
     end
   }, -- enable lsp
 
+  { "williamboman/nvim-lsp-installer",
+    cmd = "LspInstallInfo",
+  }, -- simple lsp installer
+
   { "folke/trouble.nvim",
-    dependencies = { "kyazdani42/nvim-web-devicons" },
+    lazy = true,
+    cmd = "TroubleToggle",
     config = function() require("stevenp2.plugins.lsp.trouble").setup() end
   },
 
   -- cmp
-  {"hrsh7th/nvim-cmp",
+  { "hrsh7th/nvim-cmp",
+    lazy = true,
+    event = "InsertEnter",
+    dependencies = {
+      "windwp/nvim-autopairs",
+      "hrsh7th/cmp-buffer", -- buffer completions
+      "hrsh7th/cmp-path", -- path completions
+      "hrsh7th/cmp-cmdline", -- cmdline completions
+      "hrsh7th/cmp-nvim-lsp", -- lsp completions
+      "hrsh7th/cmp-nvim-lua"  -- lua completions
+    },
     config = function() require("stevenp2.plugins.lsp.cmp").setup() end
   },
-  "hrsh7th/cmp-buffer", -- buffer completions
-  "hrsh7th/cmp-path", -- path completions
-  "hrsh7th/cmp-cmdline", -- cmdline completions
-  "saadparwaiz1/cmp_luasnip", -- snippet completions
-  "hrsh7th/cmp-nvim-lsp", -- lsp completiona
-  "hrsh7th/cmp-nvim-lua", -- lua completiona
+
 
   -- snippets
-  "L3MON4D3/LuaSnip", --snippet engine
-  "rafamadriz/friendly-snippets", -- a bunch of snippets to use
+  { "L3MON4D3/LuaSnip",
+    event = "InsertEnter",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip", -- lua snippets
+      "rafamadriz/friendly-snippets", -- a bunch of snippets to use
+    }
+  }, --snippet engine
+
 
   -------------------------------
   -- Languages
@@ -154,28 +215,27 @@ return lazy.setup({
   -- Latex
   { "lervag/vimtex",
     ft = { "tex", "bib", },
-    dependencies = {
-      "neovim/nvim-lspconfig"
-    }
   },
 
   -- Rust
-  {"simrat39/rust-tools.nvim",
-    dependencies = {
-      "neovim/nvim-lspconfig"
-    }
+  { "simrat39/rust-tools.nvim",
+    ft = { "rs" },
   },
 
   -------------------------------
   -- Treesitter
   -------------------------------
-  {"nvim-treesitter/nvim-treesitter",
+  { "nvim-treesitter/nvim-treesitter",
     config = function() require("stevenp2.plugins.treesitter.treesitter").setup() end
   },
-  "JoosepAlviste/nvim-ts-context-commentstring", -- comment string based on context
+
+  { "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = true
+  }, -- comment string based on context
 
   -- autopairs
-  {"windwp/nvim-autopairs",
+  { "windwp/nvim-autopairs",
+    event = "InsertEnter",
     config = function() require("stevenp2.plugins.treesitter.autopairs").setup() end
   }, -- autopairing of parens, braces, etc.
 
@@ -183,10 +243,14 @@ return lazy.setup({
   -------------------------------
   -- others
   -------------------------------
+  { "kyazdani42/nvim-web-devicons",
+    dependencies = {
+      "goolord/alpha-nvim",
+    }
+  }, -- icons used in plugins
+
   "nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
-  "nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
-  "kyazdani42/nvim-web-devicons", -- icons used in plugins
   "moll/vim-bbye", -- allow for deletion of buffer
   "MunifTanjim/nui.nvim", -- UI component library for neovim
 
-})
+}, lazy_config)
