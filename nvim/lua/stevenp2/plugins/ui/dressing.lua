160645117e0f284d4c960e6fb22d35ab2b6b614b
steven <s.phung808@gmail.com>
2025-07-17 22:10:31 +1000

    remove anchor

--- nvim/lua/stevenp2/plugins/ui/dressing.lua
+++ nvim/lua/stevenp2/plugins/ui/dressing.lua
@@ -38,8 +38,6 @@ function M.setup()
       -- When true, input will start in insert mode.
       start_in_insert = true,
 
-      -- These are passed to nvim_open_win
-      anchor = "SW",
       border = "rounded",
       -- 'editor' and 'win' will default to being centered
       relative = "cursor",
@@ -129,8 +127,6 @@ function M.setup()
 
       -- Options for built-in selector
       builtin = {
-        -- These are passed to nvim_open_win
-        anchor = "NW",
         border = "rounded",
         -- 'editor' and 'win' will default to being centered
         relative = "editor",
