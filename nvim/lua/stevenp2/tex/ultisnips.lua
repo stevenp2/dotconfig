e926e23f9be11c63817c5c34c21d0a2f5701c678
steven <s.phung808@gmail.com>
2025-07-17 22:07:32 +1000

    tex snippets updated and ultisnips configuration updated

--- nvim/lua/stevenp2/tex/ultisnips.lua
+++ nvim/lua/stevenp2/tex/ultisnips.lua
@@ -3,6 +3,6 @@ return function()
     vim.g.UltiSnipsEnableSnipMate = 0
     vim.g.UltiSnipsRemoveSelectModeMappings = 0
     vim.g.UltiSnipsExpandTrigger = "<tab>"
-    vim.g.UltiSnipsJumpForwardTrigger = "<tab>"
-    vim.g.UltiSnipsJumpBackwardTrigger = "<S-tab>"
+    vim.g.UltiSnipsJumpForwardTrigger = "C-h"
+    vim.g.UltiSnipsJumpBackwardTrigger = "C-l"
 end
