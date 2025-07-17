0108c0ea07f4cb2f2e5a4019e41556e684672c9e
steven <s.phung808@gmail.com>
2025-07-17 22:07:33 +1000

    refactor: removed newline text as it made nvim feel too clustered

--- nvim/lua/stevenp2/indent-blankline.lua
+++ nvim/lua/stevenp2/indent-blankline.lua
@@ -6,7 +6,7 @@ end
 
 vim.opt.list = true
 vim.opt.listchars:append "space:⋅"
-vim.opt.listchars:append "eol:↴"
+--[[ vim.opt.listchars:append "eol:↴" ]]
 
 indent_ok.setup({
   show_end_of_line = true,
