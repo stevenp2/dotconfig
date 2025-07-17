b2361a02425a961c2575cfb7522832d41ae069d4
steven <s.phung808@gmail.com>
2025-07-17 22:10:46 +1000

    cleanup ui

--- nvim/lua/stevenp2/plugins/ui/init.lua
+++ nvim/lua/stevenp2/plugins/ui/init.lua
@@ -3,8 +3,6 @@ require("stevenp2.plugins.ui.gitsigns")
 require("stevenp2.plugins.ui.scrollbar")
 require("stevenp2.plugins.ui.neoscroll")
 require("stevenp2.plugins.ui.indent-blankline")
-require("stevenp2.plugins.ui.incline")
-require("stevenp2.plugins.ui.dressing")
 require("stevenp2.plugins.ui.lualine")
 
 
