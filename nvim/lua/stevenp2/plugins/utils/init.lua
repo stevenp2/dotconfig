0775c674c0f37134d5cb024b9de7d8a33f83379a
steven <s.phung808@gmail.com>
2025-07-17 22:08:29 +1000

    which-key not implemented yet

--- nvim/lua/stevenp2/plugins/utils/init.lua
+++ nvim/lua/stevenp2/plugins/utils/init.lua
@@ -3,5 +3,5 @@ require("stevenp2.plugins.utils.nvim-tree")
 require("stevenp2.plugins.utils.telescope")
 require("stevenp2.plugins.utils.toggleterm")
 require("stevenp2.plugins.utils.urlview")
-require("stevenp2.plugins.utils.which-key")
 require("stevenp2.plugins.utils.doc_preview")
+--[[ require("stevenp2.plugins.utils.which-key") ]]
