2b067f13ac68233f9e3af5a2d44fc0a600edfe10
steven <s.phung808@gmail.com>
2025-07-17 22:08:28 +1000

    setup scripts

---
+++ setup-utils.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+is_linux() {
+  [[ "$(uname)" == "Linux" ]]
+}
+
+is_mac() {
+  # [[ "$OSTYPE" == "darwin"* ]]
+  [[ "$(uname)" == "Darwin" ]]
+}
+
+if is_mac; then
+  export OS_PYTHON="python3"
+  export OS_PIP="pip3"
+else
+  export OS_PYTHON="python"
+  export OS_PIP="pip"
+fi
+
+confirm() {
+  action=$1
+  # TODO: maybe want user to confirm by pressing enter as well?
+  read -p "Would you like to $action? (y/n): " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    return 0
+  elif [[ $REPLY =~ ^[Nn]$ ]]; then
+    return 1
+  else
+    confirm "$action"
+  fi
+}
+
+check_dependency() {
+  command -v "$1" > /dev/null 2>&1
+}
+
+backup_dst_config() {
+  dst=$1
+  as_user=$2
+  if [ -e "$dst" ]; then
+    backup="$dst.$(date +%s)"
+    echo "Backing up $dst to $backup"
+    $as_user mv "$dst" "$backup"
+  fi
+}
+
+# TODO: mkdir -p
+link_config() {
+  src=$1
+  dst=$2
+  as_root=${3:-false}
+  copy_only=${4:-false}
+  [[ $as_root = true ]] && as_user="sudo" || as_user=""
+  [[ $copy_only = true ]] && command="cp" || command="ln -s"
+  # create and move to backup if exists
+  backup_dst_config $dst $as_user
+  $as_user $command "$src" "$dst"
+}
